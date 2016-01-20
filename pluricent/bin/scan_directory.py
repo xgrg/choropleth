4ff5316532e303309600ea457f81f11989ce85de
Grégory Operto <gregory.operto@cea.fr>
2016-01-20 20:20:47 +0100

    [ENH] Added first ad-hoc scripts to identify and populate database

---
+++ pluricent/bin/scan_directory.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+''' Contains code supposed to parse directories and identify files
+that will populate the clean database. Mostly ad-hoc scripts
+performing quick and dirty operations
+
+*** scandir_BVdatabase : to run on BVddatabase directories
+(make use of brainvisa.checkbase)
+'''
+
+def size_to_human(full_size):
+  size = full_size
+  if size >= 1024:
+    unit = 'KiB'
+    size /= 1024.0
+    if size >= 1024:
+      unit = 'MiB'
+      size /= 1024.0
+      if size >= 1024:
+        unit = 'GiB'
+        size /= 1024.0
+        if size >= 1024:
+          unit = 'TiB'
+          size /= 1024.0
+    s = '%.2f' % (size,)
+    if s.endswith( '.00' ): s = s[:-3]
+    elif s[-1] == '0': s = s[:-1]
+    return s + ' ' + unit + ' (' + str(full_size) + ')'
+  else:
+    return str(size)
+
+stat_attributes = ('st_atime', 'st_ctime', 'st_gid', 'st_mode', 'st_mtime',
+               'st_nlink', 'st_size', 'st_uid', 'st_blksize', 'st_blocks',
+               'st_dev','st_rdev', 'st_ino')
+
+
+def scandir_BVdatabase(studydir):
+    '''Objectif: trouver sujets, centres et images T1 nifti'''
+    ''' Strategie : BVdatabase '''
+    import os.path as osp
+    import os
+
+    actions = []
+    assert(osp.isdir(studydir))
+    studydir = studydir.rstrip('/')
+    assert(osp.split(studydir)[-1] == 'BVdatabase')
+
+    from brainvisa import checkbase as cb
+    m = cb.MorphologistCheckbase(studydir)
+
+    subjects = m.get_flat_subjects()
+
+    actions.extend([('add_subject', s) for s in subjects])
+
+    for subject in subjects:
+      files = m.get_subject_hierarchy_files(subject)
+
+      for k,v in files.items():
+        for each in v:
+           fp = cb.getfilepath(k, each)
+           if osp.isfile(fp):
+              if k == 'raw':
+                 print fp, 'identified as', k
+                 actions.append(('add_image', subject, fp))
+    return actions
+
+
+    # estimating directory size
+    cumulative_size = 0
+    for root, dirs, files in os.walk(studydir):
+        for f in files:
+            cumulative_size += os.stat(osp.join(root,f)).st_size
+
+    print size_to_human(cumulative_size)
+
+
+def move_raw(actions, destdir):
+    import os.path as osp
+    import os,shutil
+    assert(osp.exists(destdir))
+
+    for k, subject, fp in actions:
+        if k=='add_image':
+            d = osp.join(destdir,subject,'anatomy')
+            os.makedirs(d)
+            os.system('cp %s %s'%(fp, ))
+
+
