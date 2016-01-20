0663b64199c931f458dbfd66c137193c55ebc571
Grégory Operto <gregory.operto@cea.fr>
2016-01-20 20:21:28 +0100

    [ENH] Adding first subjects, first folders and general info

--- pluricent/python/pluricent/__init__.py
+++ pluricent/python/pluricent/__init__.py
@@ -7,8 +7,11 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-def initialize_database(fn = 'pluricent.db'):
+def initialize_database(fn = 'pluricent.db', datasource = 'pluricent/'):
     ''' Initializes a sqlite database with empty tables e.g. Action, Study, Subject, Center, Scanner, T1Image '''
+    import os.path as osp
+    import os
+    assert(osp.exists(datasource) and len(os.listdir(datasource)) == 0)
 
     from sqlalchemy import create_engine, Table
     engine = create_engine('sqlite:///%s'%fn, encoding='utf-8')
@@ -19,16 +22,23 @@ def initialize_database(fn = 'pluricent.db'):
     Base = declarative_base()
     Base.metadata.bind = engine
 
+    class General(Base):
+        __tablename__ = 'general'
+        id = Column(Integer, primary_key=True)
+        key = Column(String(25), nullable=False, unique=True)
+        value = Column(String(100), nullable=False, unique=True)
+
     class Study(Base):
         __tablename__ = 'study'
         id = Column(Integer, primary_key=True)
         name = Column(String(25), nullable=False, unique=True)
+        directory = Column(String(100), nullable=False, unique=True)
 
     class Subject(Base):
         __tablename__ = 'subject'
         id = Column(Integer, primary_key=True)
         identifier = Column(String(25), nullable=False)
-        study_id = Column(Integer, ForeignKey('study.id'))
+        study_id = Column(Integer, ForeignKey('study.id'), nullable=False)
         birth_date = Column(String(10))
         sex = Column(String(1))
         study = relationship(Study)
@@ -57,16 +67,22 @@ def initialize_database(fn = 'pluricent.db'):
     class T1Image(Base):
         __tablename__ = 't1image'
         id = Column(Integer, primary_key=True)
-        subject_id = Column(Integer, ForeignKey('subject.id'))
+        subject_id = Column(Integer, ForeignKey('subject.id'), nullable=False)
         acquisition_date = Column(String(10))
         scanner_id = Column(Integer, ForeignKey('scanner.id'))
         quality_report = Column(String(25))
         quality_score = Column(Integer, doc="General quality score summarizing all various aspects of the image")
         subject = relationship(Subject)
         scanner = relationship(Scanner)
-
+        path = Column(String(100), nullable=False, unique=True)
 
     Base.metadata.create_all()
+    from sqlalchemy.orm import sessionmaker
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    new_general = General(key='datasource', value=datasource)
+    session.add(new_general)
+    session.commit()
     return engine
 
 
@@ -88,11 +104,45 @@ def create_session(fn = 'pluricent.db'):
 
 
 def add_study(session, name):
+    import os.path as osp
+    import os
     from pluricent.models import Study
-    new_study = Study(name=name)
+    ds = datasource(session)
+    assert(osp.isdir(ds))
+    s = studies(session)
+    directory = 'ds%05d'%(len(s)+1)
+    assert(not osp.exists(osp.join(ds, directory)))
+    os.mkdir(osp.join(ds, directory))
+
+    new_study = Study(id=(len(s)+1), name=name, directory=directory)
+
     session.add(new_study)
     session.commit()
 
+def add_subjects(session, subjects, study):
+    import os.path as osp
+    import os
+    from pluricent.models import Study, Subject
+    ds = datasource(session) 
+    study_id = study_id(session, study)
+    studydir = session.query(Study.directory).filter(Study.id==study_id).one()[0]
+    assert(osp.isdir(osp.join(ds, studydir)))
+    for s in subjects:
+        assert(not osp.exists(osp.join(ds, studydir, s)))
+        os.mkdir(osp.join(ds, studydir, s))
+        new_subject = Subject(identifier=s, study_id=study_id)
+        session.add(new_subject)
+    session.commit()
+
+def study_id(session, study):
+    from models import Study
+    return session.query(Study.id).filter(Study.name==study).one()[0]
+
+
+def subject_id(session, study_name, identifier):
+    from models import Subject
+    return session.query(Subject.id).filter(Subject.identifier==identifier).one()[0]
+    
 
 def add_center(session, name, location=None):
     #FIXME check if location is provided
@@ -104,20 +154,16 @@ def studies(session):
     from pluricent.models import Study
     return [each.name for each in session.query(Study).all()]
 
-def populate_study(studydir, fn = 'pluricent.db'):
-    import os.path as osp
+def datasource(session):
+    from pluricent.models import General
+    return session.query(General.value).filter(General.key=='datasource').one()[0]
 
-    session = create_session(fn)
+def add_t1image(session, path, study, subject):
+    from pluricent.models import T1Image
 
-    # checks if study already exists in the database
-    s = studies(session)
-
-    dirname = osp.split(studydir)[-1]
-    actions = []
-    if not dirname in s:
-        actions.append(('add_study', dirname))
-
-    make_actions(session, actions)
+    new_t1image = T1Image(subject_id=subject_id(session, study, subject), study_id=study_id(session, study), path=path)
+    session.add(new_t1image)
+    session.commit()
 
 def make_actions(session, actions):
     from time import gmtime, strftime
--- pluricent/python/pluricent/models.py
+++ pluricent/python/pluricent/models.py
@@ -4,49 +4,56 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship
 from pluricent import Base
 
+class General(Base):
+  __tablename__ = 'general'
+  id = Column(Integer, primary_key=True)
+  key = Column(String(25), nullable=False, unique=True)
+  value = Column(String(100), nullable=False, unique=True)
+
 class Study(Base):
-    __tablename__ = 'study'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(25), nullable=False, unique=True)
+  __tablename__ = 'study'
+  id = Column(Integer, primary_key=True)
+  name = Column(String(25), nullable=False, unique=True)
+  directory = Column(String(100), nullable=False, unique=True)
 
 class Subject(Base):
-    __tablename__ = 'subject'
-    id = Column(Integer, primary_key=True)
-    identifier = Column(String(25), nullable=False)
-    study_id = Column(Integer, ForeignKey('study.id'))
-    birth_date = Column(String(10))
-    sex = Column(String(1))
-    study = relationship(Study)
+  __tablename__ = 'subject'
+  id = Column(Integer, primary_key=True)
+  identifier = Column(String(25), nullable=False)
+  study_id = Column(Integer, ForeignKey('study.id'), nullable=False)
+  birth_date = Column(String(10))
+  sex = Column(String(1))
+  study = relationship(Study)
 
 class Action(Base):
-    __tablename__ = 'action'
-    id = Column(Integer, primary_key=True)
-    action = Column(String(250), nullable=False)
-    timestamp = Column(String(19), nullable=False)
+  __tablename__ = 'action'
+  id = Column(Integer, primary_key=True)
+  action = Column(String(250), nullable=False)
+  timestamp = Column(String(19), nullable=False)
 
 class Center(Base):
-    __tablename__ = 'center'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(25), nullable=False)
-    location = Column(String(50))
+  __tablename__ = 'center'
+  id = Column(Integer, primary_key=True)
+  name = Column(String(25), nullable=False)
+  location = Column(String(50))
 
 class Scanner(Base):
-    __tablename__ = 'scanner'
-    id = Column(Integer, primary_key=True)
-    intensity_field = Column(Float)
-    manufacturer = Column(String(25))
-    center_id = Column(Integer, ForeignKey('center.id'))
-    service_starting_date = Column(String(10), doc="Date when the system was up and running.")
-    center = relationship(Center)
+  __tablename__ = 'scanner'
+  id = Column(Integer, primary_key=True)
+  intensity_field = Column(Float)
+  manufacturer = Column(String(25))
+  center_id = Column(Integer, ForeignKey('center.id'))
+  service_starting_date = Column(String(10), doc="Date when the system was up and running.")
+  center = relationship(Center)
 
 class T1Image(Base):
-    __tablename__ = 't1image'
-    id = Column(Integer, primary_key=True)
-    subject_id = Column(Integer, ForeignKey('subject.id'))
-    acquisition_date = Column(String(10))
-    scanner_id = Column(Integer, ForeignKey('scanner.id'))
-    quality_report = Column(String(25))
-    quality_score = Column(Integer, doc="General quality score summarizing all various aspects of the image")
-    subject = relationship(Subject)
-    scanner = relationship(Scanner)
-
+  __tablename__ = 't1image'
+  id = Column(Integer, primary_key=True)
+  subject_id = Column(Integer, ForeignKey('subject.id'), nullable=False)
+  acquisition_date = Column(String(10))
+  scanner_id = Column(Integer, ForeignKey('scanner.id'))
+  quality_report = Column(String(25))
+  quality_score = Column(Integer, doc="General quality score summarizing all various aspects of the image")
+  subject = relationship(Subject)
+  scanner = relationship(Scanner)
+  path = Column(String(100), nullable=False, unique=True)
