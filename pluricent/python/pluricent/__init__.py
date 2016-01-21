5b555eb4b2131be8769ed391c9aaa1e2fe8b5eaa
Grégory Operto <gregory.operto@cea.fr>
2016-01-21 10:09:47 +0100

    [REF] Breaking classes across files

--- pluricent/python/pluricent/__init__.py
+++ pluricent/python/pluricent/__init__.py
@@ -2,80 +2,26 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy import Column, ForeignKey, Integer, String, Float
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+import base
+Base = base.Base
 
 def initialize_database(fn = 'pluricent.db', datasource = 'pluricent/'):
     ''' Initializes a sqlite database with empty tables e.g. Action, Study, Subject, Center, Scanner, T1Image '''
     import os.path as osp
     import os
+
     assert(osp.exists(datasource) and len(os.listdir(datasource)) == 0)
 
+    print 'deleting all contents from %s'%fn
     from sqlalchemy import create_engine, Table
     engine = create_engine('sqlite:///%s'%fn, encoding='utf-8')
-    Base = declarative_base()
     Base.metadata.bind = engine
     Base.metadata.reflect()
     Base.metadata.drop_all()
-    Base = declarative_base()
-    Base.metadata.bind = engine
+    Base.metadata.clear()
 
-    class General(Base):
-        __tablename__ = 'general'
-        id = Column(Integer, primary_key=True)
-        key = Column(String(25), nullable=False, unique=True)
-        value = Column(String(100), nullable=False, unique=True)
-
-    class Study(Base):
-        __tablename__ = 'study'
-        id = Column(Integer, primary_key=True)
-        name = Column(String(25), nullable=False, unique=True)
-        directory = Column(String(100), nullable=False, unique=True)
-
-    class Subject(Base):
-        __tablename__ = 'subject'
-        id = Column(Integer, primary_key=True)
-        identifier = Column(String(25), nullable=False)
-        study_id = Column(Integer, ForeignKey('study.id'), nullable=False)
-        birth_date = Column(String(10))
-        sex = Column(String(1))
-        study = relationship(Study)
-
-    class Action(Base):
-        __tablename__ = 'action'
-        id = Column(Integer, primary_key=True)
-        action = Column(String(250), nullable=False)
-        timestamp = Column(String(19), nullable=False)
-
-    class Center(Base):
-        __tablename__ = 'center'
-        id = Column(Integer, primary_key=True)
-        name = Column(String(25), nullable=False)
-        location = Column(String(50))
-
-    class Scanner(Base):
-        __tablename__ = 'scanner'
-        id = Column(Integer, primary_key=True)
-        intensity_field = Column(Float)
-        manufacturer = Column(String(25))
-        center_id = Column(Integer, ForeignKey('center.id'))
-        service_starting_date = Column(String(10), doc="Date when the system was up and running.")
-        center = relationship(Center)
-
-    class T1Image(Base):
-        __tablename__ = 't1image'
-        id = Column(Integer, primary_key=True)
-        subject_id = Column(Integer, ForeignKey('subject.id'), nullable=False)
-        acquisition_date = Column(String(10))
-        scanner_id = Column(Integer, ForeignKey('scanner.id'))
-        quality_report = Column(String(25))
-        quality_score = Column(Integer, doc="General quality score summarizing all various aspects of the image")
-        subject = relationship(Subject)
-        scanner = relationship(Scanner)
-        path = Column(String(100), nullable=False, unique=True)
-
+    from models import General, Study, Subject, Action, Center, Scanner, T1Image
     Base.metadata.create_all()
     from sqlalchemy.orm import sessionmaker
     DBSession = sessionmaker(bind=engine)
@@ -94,7 +40,6 @@ def create_engine(fn = 'pluricent.db'):
 
 def create_session(fn = 'pluricent.db'):
     engine = create_engine(fn)
-    Base = declarative_base()
     Base.metadata.bind = engine
     Base.metadata.reflect()
     from sqlalchemy.orm import sessionmaker
