c3bc41d7ab448f2d1c5861d841d0d9c8b035ab75
Grégory Operto <goperto@gmail.com>
2016-02-04 17:02:47 +0100

    [ENH] Tornado web server

---
+++ pluricent/python/pluricent/web/__init__.py
@@ -0,0 +1,82 @@
+import tornado.auth
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import settings
+
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+class MainHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        username = tornado.escape.xhtml_escape(self.current_user)
+        print self.current_user, username
+        self.render("html/index.html", username = username)
+
+class AuthLoginHandler(BaseHandler):
+    def get(self):
+        try:
+            errormessage = self.get_argument("error")
+        except:
+            errormessage = ""
+        self.render("html/login.html", errormessage = errormessage)
+
+    def check_permission(self, password, username):
+        if username == "admin" and password == "admin":
+            return True
+        return False
+
+    def post(self):
+        username = self.get_argument("username", "")
+        password = self.get_argument("password", "")
+        auth = self.check_permission(password, username)
+        if auth:
+            self.set_current_user(username)
+            self.redirect(self.get_argument("next", u"/"))
+        else:
+            error_msg = u"?error=" + tornado.escape.url_escape("Login incorrect")
+            self.redirect(u"/auth/login/" + error_msg)
+
+    def set_current_user(self, user):
+        if user:
+            self.set_secure_cookie("user", tornado.escape.json_encode(user))
+        else:
+            self.clear_cookie("user")
+
+class AuthLogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("user")
+        self.redirect(self.get_argument("next", "/"))
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/auth/login/", AuthLoginHandler),
+            (r"/auth/logout/", AuthLogoutHandler),
+        ]
+        s = {
+            "template_path":settings.TEMPLATE_PATH,
+            "static_path":settings.STATIC_PATH,
+            "debug":settings.DEBUG,
+            "cookie_secret": settings.COOKIE_SECRET,
+            "login_url": "/auth/login/"
+        }
+        tornado.web.Application.__init__(self, handlers, **s)
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
